aa2b955889e9642cd8bb5b0ef0d1881b792bdbac
yingcyl <chiayinglow@gmail.com>
2023-06-26 18:09:24 +0100

    Update mongoDB connection

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 const userRoutes = require("./routes/users");
 
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
