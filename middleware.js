2a78b10f9188d4b21eac92e19db7a408ecdbf832
yingcyl <chiayinglow@gmail.com>
2023-06-26 12:30:35 +0100

    Add middleware file

---
+++ middleware.js
@@ -0,0 +1,62 @@
+const Campground = require("./models/campground");
+const { campgroundSchema, reviewSchema } = require("./schemas.js");
+const ExpressError = require("./utils/ExpressError");
+
+module.exports.isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
+    req.flash("error", "You must be logged in!");
+    return res.redirect("/login");
+  }
+  next();
+};
+
+// this middleware is to store the returnTo url in res.locals before passport clears the session after authenticating a user
+module.exports.storeReturnTo = (req, res, next) => {
+  // if there returnTo in session is not empty, save returnTo to locals
+  if (req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo;
+  }
+  next();
+};
+
+// middleware to authenticate if logged in user has permission to make changes to campgrounds/reviews
+module.exports.isAuthor = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground.author.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to do that!");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
+
+module.exports.validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+module.exports.validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to do that!");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
